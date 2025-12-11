e564513ef55c45e075287c45bd23efc8bb31f986
김준상 <rlawnstkd26@gmail.com>
2025-12-11 19:39:10 +0900

    202413132 김준상 stack.c 수정
    202413132 김준상 stack.c 수정

--- stack.c
+++ stack.c
@@ -3,6 +3,7 @@
 #include "stack.h"
 #include <string.h>
 
+printf("hello world")
 Stack * create_stack(int initial_size)
 {
 	Stack * s = (Stack *)malloc(sizeof(Stack));
@@ -73,4 +74,5 @@ static void reallocate(Stack * stack)
 	free(stack->contents);
 	stack->contents = tmp;
 }
+
 
